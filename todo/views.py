edbc8461adc5a07d442b6278f29708d6dfd18c09
Luksos9 <lukaszszumilas9@wp.pl>
2021-07-22 06:51:39 +0200

    Added signup user

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
 
 # Create your views here.
+def signupuser(request):
+    """It handles signup user, checks if passwords do match if so it saves user to database and logins,
+     if they dont or user exist,informs by returning error"""
+
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:  # POST
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Username taken'})
+
+        else:
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Passwords did not match'})
+
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
